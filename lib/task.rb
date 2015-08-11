a2467b26a8bae97cf13925b621ea098c08e4a9d7
Janice Laset-Parkerson & Shannon Mulloy <student@epicodus.com>
2015-08-11 09:24:36 -0700

    reveal all tasks

--- lib/task.rb
+++ lib/task.rb
@@ -1,9 +1,15 @@
 class Task
+  @@all_tasks = []
+
   define_method(:initialize) do |description|
     @description = description
   end
 
   define_method(:description) do
     @description
+  end
+
+  define_singleton_method(:all) do
+    @@all_tasks
   end
 end
